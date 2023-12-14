aa8b93ba2c741cc8c299a4c49f9409bbe25974e8
Rodrigo Viloria <rnviloria@hotmail.com>
2023-12-14 14:38:07 -0300

    Usando plantillas

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -5,7 +5,14 @@ from django.http import HttpResponse
 def crear_curso(request):
     curso = Curso(nombre="Python", camada=47785)
     curso.save()
+    contexto = { "curso": curso}
 
-    return HttpResponse(curso.nombre)
+
+    return render(request, 'index.html', contexto)
+
+def show_html(request):
+    cursos = Curso.objects.first()
+    contexto = {"curso": cursos, "nombre": "Rodrigo"}
+    return render(request, 'index.html',contexto)
 
 
--- ProyectoCoder/urls.py
+++ ProyectoCoder/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from AppCoder.views import crear_curso
 
+
 urlpatterns = [
-    path("agregar_curso/", crear_curso),
+
+    path('app/', include('AppCoder.urls')),
     path('admin/', admin.site.urls),
 ]
